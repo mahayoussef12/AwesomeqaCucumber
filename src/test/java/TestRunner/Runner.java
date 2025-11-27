fa7fa765ad6fbed7d4775fc4306d7f62c69fad92
mahayoussef12 <mayouss30@gmail.com>
2025-11-27 15:50:47 +0100

    jenkins

--- src/test/java/TestRunner/Runner.java
+++ src/test/java/TestRunner/Runner.java
@@ -5,10 +5,10 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
-        glue = {"StepDefinitions"},
-        plugin = {"pretty", "html:target/cucumber/rapport.html"}
+@CucumberOptions(features = "src/test/java/Features/Logout.feature" ,
+        glue = "StepDefinitions",
+
+        plugin = {"pretty","html:target\\cucumber\\rapport.html"}
 )
 
 public class Runner {
